4ccacba7e1b8dda52be719d79adeeccc94555529
SOURAV PAUL <souravpaul@protonmail.com>
2020-05-29 12:03:58 +0530

    Working Face and Expression Detection

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const video = document.getElementById('video');
 
-const constraints = {audio: false , video: true};
-
 const startVideo = () => {
-    navigator.mediaDevices.getUserMedia(constraints)
+    navigator.mediaDevices.getUserMedia({video: true})
     .then(function(mediaStream) {
         video.srcObject = mediaStream;
-        video.srcObject = mediaStream;
-        video.onloadedmetadata = function(e) {
-            video.play();
-        };
     })
     .catch(function(err){
-        console.error(err);
+        console.log(err);
     });
 }
-startVideo();
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo);
+
+video.addEventListener('play', ()=> {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displayField = { width: video.width, height: video.height};
+    faceapi.matchDimensions(canvas, displayField);
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).
+            withFaceLandmarks().withFaceExpressions();
+        console.log(detections);
+        const resizedResults = faceapi.resizeResults(detections, displayField);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedResults);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedResults);
+        faceapi.draw.drawFaceExpressions(canvas, resizedResults);
+    },1)
+})
